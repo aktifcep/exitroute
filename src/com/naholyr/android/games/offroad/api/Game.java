7526852632de89ecbfaf944656f2e0681b177d56
naholyr <naholyr@8f207d18-1434-11df-8dfc-8707880b9d12>
2010-02-15 23:30:38 +0000

    REFS #1 - Trackball  * Basic handling for trackball and arrows  * Refactoring

--- src/com/naholyr/android/games/offroad/api/Game.java
+++ src/com/naholyr/android/games/offroad/api/Game.java
@@ -7,14 +7,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.view.View;
 import android.view.ViewGroup;
 
 import com.naholyr.android.games.offroad.R;
 import com.naholyr.android.games.offroad.view.GameView;
 import com.naholyr.android.games.offroad.view.TargetView;
 
-public class Game {
+public class Game implements TargetView.Listener {
 
 	public GameParameters params;
 
@@ -28,7 +27,14 @@ public class Game {
 
 	public ErrorListener errorListener = null;
 
-	private TargetView[] targetViews;
+	// Saved portion of view for fast restore
+	private int savedX;
+	private int savedY;
+	private int savedW;
+	private int savedH;
+	private Canvas savedCanvas;
+	private Bitmap savedBitmap;
+	private Activity savedLauncher;
 
 	public Game(GameParameters gameParameters, int playerIndex) {
 		params = gameParameters;
@@ -62,182 +68,28 @@ public class Game {
 
 	public void draw(GameView gameView) {
 		params.map.draw(gameView, true);
-		params.map.getImageView().invalidate();
+		params.map.getView().invalidate();
 		params.map.drawPlayers(gameView.getContext(), params.players);
 	}
 
 	public void run(Activity launcher) {
 		Player player = getCurrentPlayer();
 		params.map.focusPlayer(player);
-		Position[] targets = player.getTargets(params.players);
 
 		// Remove all existing target views
-		if (targetViews != null) {
-			for (int i = 0; i < targetViews.length; i++) {
-				if (targetViews[i] != null) {
-					((ViewGroup) targetViews[i].getParent()).removeView(targetViews[i]);
-				}
-			}
-		}
+		params.map.getView().removeAllTargetViews();
+
+		// Backup portion of the view, to restore it easily
+		savedX = params.map.getRealX(Math.max(0, player.position.x - Math.abs(player.speed.x) - player.maxAcceleration));
+		savedY = params.map.getRealY(Math.max(0, player.position.y - Math.abs(player.speed.y) - player.maxAcceleration));
+		savedW = params.map.getRealX(2 * (Math.abs(player.speed.x) + player.maxAcceleration) + 1);
+		savedH = params.map.getRealY(2 * (Math.abs(player.speed.y) + player.maxAcceleration) + 1);
+		savedCanvas = params.map.getView().mImageView.mCanvas;
+		savedBitmap = Bitmap.createBitmap(params.map.getView().mImageView.mBitmap, savedX, savedY, savedW, savedH);
+		savedLauncher = launcher;
 
 		// Generate target views
-		targetViews = new TargetView[targets.length];
-		for (int i = 0; i < targetViews.length; i++) {
-			targetViews[i] = params.map.drawTarget(launcher, targets[i]);
-			Player playerAt = params.map.getPlayerAt(targets[i].x, targets[i].y, params.players);
-			if (playerAt == null || playerAt == player) {
-				targetViews[i].setVisibility(View.VISIBLE);
-			} else {
-				targetViews[i].setVisibility(View.GONE);
-			}
-		}
-
-		// Add behaviors
-		for (int i = 0; i < targetViews.length; i++) {
-			if (targetViews[i] != null) {
-				setTargetViewBehaviors(launcher, targetViews, i);
-			}
-		}
-
-		return;
-	}
-
-	private void setTargetViewBehaviors(final Activity launcher, final TargetView[] views, int i) {
-		final Player player = getCurrentPlayer();
-		final int savedX = params.map.getRealX(Math.max(0, player.position.x - Math.abs(player.speed.x) - player.maxAcceleration));
-		final int savedY = params.map.getRealY(Math.max(0, player.position.y - Math.abs(player.speed.y) - player.maxAcceleration));
-		final int savedW = params.map.getRealX(2 * (Math.abs(player.speed.x) + player.maxAcceleration) + 1);
-		final int savedH = params.map.getRealY(2 * (Math.abs(player.speed.y) + player.maxAcceleration) + 1);
-		final Canvas canvas = params.map.getImageView().mImageView.mCanvas;
-		final Bitmap savedBitmap = Bitmap.createBitmap(params.map.getImageView().mImageView.mBitmap, savedX, savedY, savedW, savedH);
-		views[i].setListener(new TargetView.Listener() {
-
-			@Override
-			public void onUnSelect(TargetView view) {
-				try {
-					view.reset(true);
-					params.map.getView(player).setAlpha(255);
-				} catch (RuntimeException e) {
-					Game.this.handleError(e);
-				}
-				return;
-			}
-
-			@Override
-			public void onSelect(TargetView view) {
-				try {
-					int x = params.map.getCoordsX(view.getLeft());
-					int y = params.map.getCoordsY(view.getTop());
-					// Unselect all other targets
-					for (int j = 0; j < views.length; j++) {
-						if (views[j] == null || views[j] == view) {
-							continue;
-						}
-						if (views[j].step == TargetView.STEP_CONFIRM) {
-							views[j].unSelect();
-						}
-					}
-					// Change target view to mark it's selected
-					view.setImageBitmap(player.icon.getBitmap());
-					view.setAlpha(255);
-					Speed speed = player.getNewSpeedForTarget(x, y);
-					float angle = Player.getOrientationAngle(speed);
-					view.rotate(angle);
-					params.map.getView(player).setAlpha(127);
-
-					// Redraw saved original bitmap rect
-					canvas.drawBitmap(savedBitmap, savedX, savedY, new Paint());
-					// Draw a thick line between position and target
-					Paint paint = new Paint();
-					paint.setStrokeWidth(4.0f);
-					paint.setColor(player.color);
-					float x1 = params.map.getRealXCenter(x);
-					float y1 = params.map.getRealYCenter(y);
-					float x2 = params.map.getRealXCenter(player.position.x);
-					float y2 = params.map.getRealYCenter(player.position.y);
-					canvas.drawLine(x1, y1, x2, y2, paint);
-					// If player is on road, and target is not accessible,
-					// display a warning
-					if (params.map.isCellAccessible(player.position.x, player.position.y) && !params.map.isCellAccessible(x, y)) {
-						Bitmap warningImg = BitmapFactory.decodeResource(view.getContext().getResources(), R.drawable.warning);
-						canvas.drawBitmap(warningImg, view.getLeft(), view.getTop(), new Paint());
-					}
-				} catch (RuntimeException e) {
-					Game.this.handleError(e);
-				}
-				return;
-			}
-
-			@Override
-			public void onConfirm(TargetView view) {
-				try {
-					int x = params.map.getCoordsX(view.getLeft());
-					int y = params.map.getCoordsY(view.getTop());
-					boolean resetSpeedAfterMove = false;
-					boolean doNotRunNextTurn = false;
-					float x1 = params.map.getRealXCenter(player.position.x);
-					float y1 = params.map.getRealYCenter(player.position.y);
-					float x2 = params.map.getRealXCenter(x);
-					float y2 = params.map.getRealYCenter(y);
-					Position[] cellsThrough = params.map.getCellsThrough(x1, y1, x2, y2);
-					if (params.map.isCellAccessible(player.position.x, player.position.y)) {
-						// Player is on road : check the path
-						// Get cells the player will get through, and check if
-						// one is inaccessible
-						for (int k = 0; k < cellsThrough.length; k++) {
-							Position cell = cellsThrough[k];
-							if (!cell.equals(player.position)) {
-								// Check if player is on road end : winner !
-								if (params.map.isCellEnd(cell.x, cell.y)) {
-									// End game
-									showWinner(Game.this, launcher, player);
-								} else if (!params.map.isCellAccessible(cell.x, cell.y)) {
-									doNotRunNextTurn = true;
-									showAlertPosition(Game.this, launcher, player, true);
-									resetSpeedAfterMove = true;
-									x = cell.x;
-									y = cell.y;
-									break;
-								}
-							}
-						}
-					} else {
-						// Player is out road, no acceleration allowed
-						resetSpeedAfterMove = true;
-						// And he cannot win ! we don't check end cell in this
-						// case
-					}
-					Canvas canvas = params.map.getImageView().mImageView.mCanvas;
-					// Redraw saved original bitmap rect
-					canvas.drawBitmap(savedBitmap, savedX, savedY, new Paint());
-					// Draw a thin line between position and target, and a
-					// marker for original position
-					Paint paint = new Paint();
-					paint.setStrokeWidth(2.0f);
-					paint.setColor(player.color);
-					paint.setStyle(Paint.Style.FILL_AND_STROKE);
-					canvas.drawLine(params.map.getRealXCenter(player.position.x), params.map.getRealYCenter(player.position.y), params.map
-							.getRealXCenter(x), params.map.getRealYCenter(y), paint);
-					canvas.drawCircle(params.map.getRealXCenter(player.position.x), params.map.getRealYCenter(player.position.y), 3.0f, paint);
-					// Move player
-					player.moveTo(x, y);
-					if (resetSpeedAfterMove) {
-						player.speed = new Speed(0, 0);
-					}
-					// Redraw
-					params.map.drawPlayer(view.getContext(), player);
-					// Next turn
-					if (!doNotRunNextTurn) {
-						Game.this.nextTurn(launcher);
-					}
-				} catch (RuntimeException e) {
-					Game.this.handleError(e);
-				}
-				return;
-			}
-
-		});
-		return;
+		params.map.getView().generateTargetViews(params, this, launcher);
 	}
 
 	public void nextTurn(Activity launcher) {
@@ -283,6 +135,130 @@ public class Game {
 		});
 		AlertDialog alert = builder.create();
 		alert.show();
+	}
+
+	@Override
+	public void onUnSelect(TargetView view) {
+		try {
+			view.reset(true);
+			params.map.getView(getCurrentPlayer()).setAlpha(255);
+		} catch (RuntimeException e) {
+			handleError(e);
+		}
+	}
+
+	@Override
+	public void onSelect(TargetView view) {
+		Player player = getCurrentPlayer();
+		TargetView[] views = params.map.getView().getTargetViews();
+		try {
+			int x = params.map.getCoordsX(view.getLeft());
+			int y = params.map.getCoordsY(view.getTop());
+			// Unselect all other targets
+			for (int j = 0; j < views.length; j++) {
+				if (views[j] == null || views[j] == view) {
+					continue;
+				}
+				if (views[j].step == TargetView.STEP_CONFIRM) {
+					views[j].unSelect();
+				}
+			}
+			// Change target view to mark it's selected
+			view.setImageBitmap(player.icon.getBitmap());
+			view.setAlpha(255);
+			Speed speed = player.getNewSpeedForTarget(x, y);
+			float angle = Player.getOrientationAngle(speed);
+			view.rotate(angle);
+			params.map.getView(player).setAlpha(127);
+
+			// Redraw saved original bitmap rect
+			savedCanvas.drawBitmap(savedBitmap, savedX, savedY, new Paint());
+			// Draw a thick line between position and target
+			Paint paint = new Paint();
+			paint.setStrokeWidth(4.0f);
+			paint.setColor(player.color);
+			float x1 = params.map.getRealXCenter(x);
+			float y1 = params.map.getRealYCenter(y);
+			float x2 = params.map.getRealXCenter(player.position.x);
+			float y2 = params.map.getRealYCenter(player.position.y);
+			savedCanvas.drawLine(x1, y1, x2, y2, paint);
+			// If player is on road, and target is not accessible,
+			// display a warning
+			if (params.map.isCellAccessible(player.position.x, player.position.y) && !params.map.isCellAccessible(x, y)) {
+				Bitmap warningImg = BitmapFactory.decodeResource(view.getContext().getResources(), R.drawable.warning);
+				savedCanvas.drawBitmap(warningImg, view.getLeft(), view.getTop(), new Paint());
+			}
+		} catch (RuntimeException e) {
+			handleError(e);
+		}
+	}
+
+	@Override
+	public void onConfirm(TargetView view) {
+		Player player = getCurrentPlayer();
+		try {
+			int x = params.map.getCoordsX(view.getLeft());
+			int y = params.map.getCoordsY(view.getTop());
+			boolean resetSpeedAfterMove = false;
+			boolean doNotRunNextTurn = false;
+			float x1 = params.map.getRealXCenter(player.position.x);
+			float y1 = params.map.getRealYCenter(player.position.y);
+			float x2 = params.map.getRealXCenter(x);
+			float y2 = params.map.getRealYCenter(y);
+			Position[] cellsThrough = params.map.getCellsThrough(x1, y1, x2, y2);
+			if (params.map.isCellAccessible(player.position.x, player.position.y)) {
+				// Player is on road : check the path
+				// Get cells the player will get through, and check if
+				// one is inaccessible
+				for (int k = 0; k < cellsThrough.length; k++) {
+					Position cell = cellsThrough[k];
+					if (!cell.equals(player.position)) {
+						// Check if player is on road end : winner !
+						if (params.map.isCellEnd(cell.x, cell.y)) {
+							// End game
+							showWinner(this, savedLauncher, player);
+						} else if (!params.map.isCellAccessible(cell.x, cell.y)) {
+							doNotRunNextTurn = true;
+							showAlertPosition(this, savedLauncher, player, true);
+							resetSpeedAfterMove = true;
+							x = cell.x;
+							y = cell.y;
+							break;
+						}
+					}
+				}
+			} else {
+				// Player is out road, no acceleration allowed
+				resetSpeedAfterMove = true;
+				// And he cannot win ! we don't check end cell in this
+				// case
+			}
+			Canvas canvas = params.map.getView().mImageView.mCanvas;
+			// Redraw saved original bitmap rect
+			canvas.drawBitmap(savedBitmap, savedX, savedY, new Paint());
+			// Draw a thin line between position and target, and a
+			// marker for original position
+			Paint paint = new Paint();
+			paint.setStrokeWidth(2.0f);
+			paint.setColor(player.color);
+			paint.setStyle(Paint.Style.FILL_AND_STROKE);
+			canvas.drawLine(params.map.getRealXCenter(player.position.x), params.map.getRealYCenter(player.position.y), params.map
+					.getRealXCenter(x), params.map.getRealYCenter(y), paint);
+			canvas.drawCircle(params.map.getRealXCenter(player.position.x), params.map.getRealYCenter(player.position.y), 3.0f, paint);
+			// Move player
+			player.moveTo(x, y);
+			if (resetSpeedAfterMove) {
+				player.speed = new Speed(0, 0);
+			}
+			// Redraw
+			params.map.drawPlayer(view.getContext(), player);
+			// Next turn
+			if (!doNotRunNextTurn) {
+				nextTurn(savedLauncher);
+			}
+		} catch (RuntimeException e) {
+			handleError(e);
+		}
 	}
 
 }
--- src/com/naholyr/android/games/offroad/view/TargetView.java
+++ src/com/naholyr/android/games/offroad/view/TargetView.java
@@ -6,11 +6,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.view.View.OnTouchListener;
 
 import com.naholyr.android.games.offroad.R;
 
-public class TargetView extends TileView implements OnTouchListener {
+public class TargetView extends TileView implements OnTouchListener, OnClickListener, OnFocusChangeListener {
 
 	public static final int STEP_SELECT = 0;
 	public static final int STEP_CONFIRM = 1;
@@ -28,6 +30,10 @@ public class TargetView extends TileView implements OnTouchListener {
 
 	private Listener _listener = null;
 
+	// Set to false by calling cancelBehaviors(), this will lead all touch,
+	// click, and focus events to be ignored
+	private boolean interactive;
+
 	public TargetView(Context context, int x, int y, int cellSize) {
 		super(context, getBitmap(context), x, y, cellSize, cellSize);
 
@@ -37,6 +43,9 @@ public class TargetView extends TileView implements OnTouchListener {
 		setClickable(true);
 
 		setOnTouchListener(this);
+		setOnClickListener(this);
+		setOnFocusChangeListener(this);
+		interactive = true;
 
 		step = STEP_SELECT;
 	}
@@ -70,35 +79,68 @@ public class TargetView extends TileView implements OnTouchListener {
 	}
 
 	public void select() {
-		if (_listener != null) {
+		if (_listener != null && interactive) {
 			_listener.onSelect(this);
 		}
 		step = STEP_CONFIRM;
 	}
 
 	public void unSelect() {
-		if (_listener != null) {
+		if (_listener != null && interactive) {
 			_listener.onUnSelect(this);
 		}
 		step = STEP_SELECT;
 	}
 
 	public void confirmSelect() {
-		if (_listener != null) {
-			_listener.onConfirm(this);
-		} else {
-			// Default behavior
-			reset(true);
+		if (interactive) {
+			if (_listener != null) {
+				_listener.onConfirm(this);
+			} else {
+				// Default behavior
+				reset(true);
+			}
 		}
 		step = STEP_END;
 	}
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
+		if (!interactive) {
+			return false;
+		}
 		if (event.getAction() == MotionEvent.ACTION_UP) {
 			click();
 		}
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (!interactive) {
+			return;
+		}
+		((TargetView) v).confirmSelect();
+	}
+
+	@Override
+	public void onFocusChange(View v, boolean focus) {
+		if (!interactive) {
+			return;
+		}
+		if (focus) {
+			((TargetView) v).select();
+		} else {
+			((TargetView) v).unSelect();
+		}
+	}
+
+	public void disableBehaviors() {
+		interactive = false;
+	}
+
+	public void enableBehaviors() {
+		interactive = false;
 	}
 
 }
